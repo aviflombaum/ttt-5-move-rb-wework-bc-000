0662557716914b1e7970233fc52d9b1161d6ec74
Avi Flombaum <avi@flombaum.com>
2018-02-14 00:29:53 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,39 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def turn(board)
+  puts "Where would you like to go 1-9?"
+  input = gets.strip
+
+  index = input_to_index(input)
+
+  if valid_move?(board, index)
+    move(board, index, "O")
+  end
+  display_board(board)
+end
+# code your #valid_move? method here
+def valid_move?(board, index)
+  # is index valid
+  # if index.between?(0, 8) 
+  #   if position_taken?(board, index)
+  #     false
+  #   else
+  #     true
+  # end
+  
+  index.between?(0,8) && !position_taken?(board,index)
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  board[index] != " "
+end
